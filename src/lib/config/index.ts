f8485a4d3f94c31779108cd142b1395f98e081a5
yersonargote <yersonargote@unicauca.edu.co>
2025-01-17 13:38:51 -0500

    Update appKit metadata URL and cleanup projectId assignment in context and config files for better clarity and usability

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -13,8 +13,7 @@ import {
 } from "@reown/appkit/react";
 import { cookieStorage, createStorage } from "wagmi";
 
-export const projectId =
-	process.env.NEXT_PUBLIC_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694"; // this is a public projectId only to use on localhost
+export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID ?? "";
 
 export const networks = [polygon] as [AppKitNetwork, ...AppKitNetwork[]];
 
