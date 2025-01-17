c1e6d93b5592a2b86cd4bdaefcee9fe3672fa64d
yersonargote <yersonargote@unicauca.edu.co>
2025-01-17 11:07:20 -0500

    Refactor: Replace custom buttons with UI component in ActionButtonList and ConnectButton, clean up Next.js config

--- src/components/connect-button.tsx
+++ src/components/connect-button.tsx
@@ -2,7 +2,8 @@
 
 import { useAccount } from "wagmi";
 
-import { useAppKitAccount } from "@reown/appkit/react";
+import { useAppKit, useAppKitAccount } from "@reown/appkit/react";
+import { Button } from "./ui/button";
 
 const compactHash = (hash: string) => {
 	return `${hash.slice(0, 7)}...${hash.slice(-5)}`;
@@ -15,13 +16,15 @@ export const ConnectButton = () => {
 	const compactAddress = compactHash(account.address || "");
 	const compactAddressWagmi = compactHash(wagmiAccount.address || "");
 
+	const { open } = useAppKit();
+
 	return (
 		<div className="column">
 			<span className="text-black">useAppKitAccount: {compactAddress}</span>
 			<span className="text-black">
 				useAccount (wagmi): {compactAddressWagmi}
 			</span>
-			<appkit-button />
+			<Button onClick={() => open()}>Open Connect Modal</Button>
 		</div>
 	);
 };
