c6fcd616f7c63b25cc529f59626d2abcacf0844b
yersonargote <yersonargote@unicauca.edu.co>
2025-01-20 13:17:46 -0500

    Add NNChat icon component and integrate it into SocialLinks for improved accessibility to Napoleon Chat

--- src/components/shared/social-links.tsx
+++ src/components/shared/social-links.tsx
@@ -1,3 +1,4 @@
+import { NNChat } from "@/components/icons/nnchat";
 import Telegram from "@/components/icons/telegram";
 import { urls } from "@/lib/constants/urls";
 import Link from "next/link";
@@ -9,6 +10,13 @@ export function SocialLinks() {
 			<div className="flex gap-2 items-center justify-center">
 				<Link href={urls.telegram} target="_blank" rel="noreferrer noopener">
 					<Telegram className="w-auto h-10" />
+				</Link>
+				<Link
+					href={urls.napoleonChat}
+					target="_blank"
+					rel="noreferrer noopener"
+				>
+					<NNChat className="w-auto h-10" />
 				</Link>
 			</div>
 		</div>
