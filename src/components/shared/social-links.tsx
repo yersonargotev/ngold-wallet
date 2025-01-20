de060dd7356deca99d8dee9862d9a1771e66342a
yersonargote <yersonargote@unicauca.edu.co>
2025-01-20 14:21:44 -0500

    Update SocialLinks component to change heading style from bold to medium for improved visual consistency

--- src/components/shared/social-links.tsx
+++ src/components/shared/social-links.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export function SocialLinks() {
 	return (
 		<div className="flex flex-col gap-2 items-center justify-center">
-			<h3 className="text-lg font-bold text-muted-foreground">Need Help?</h3>
+			<h3 className="text-lg font-medium text-muted-foreground">Need Help?</h3>
 			<div className="flex gap-2 items-center justify-center">
 				<Link href={urls.telegram} target="_blank" rel="noreferrer noopener">
 					<Telegram className="w-auto h-10" />
