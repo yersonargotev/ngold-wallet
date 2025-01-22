132a6313d21953390ae97e70dab06af143adc3f5
Yerson <yargote.gruponn@gmail.com>
2025-01-22 09:38:36 -0500

    Add output configuration and console removal in production to next.config.ts for better build optimization and debugging

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+	output: "export",
 	webpack: (config) => {
 		config.externals.push("pino-pretty", "lokijs", "encoding");
 		return config;
+	},
+	compiler: {
+		removeConsole: process.env.NODE_ENV === "production",
 	},
 };
 
