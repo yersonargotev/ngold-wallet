fa77fab257e759e455a8dd5f74e8b08f9891bedb
Yerson <yargote.gruponn@gmail.com>
2025-01-22 10:05:51 -0500

    Remove output configuration from next.config.ts to streamline the build process and enhance performance

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export",
 	webpack: (config) => {
 		config.externals.push("pino-pretty", "lokijs", "encoding");
 		return config;
